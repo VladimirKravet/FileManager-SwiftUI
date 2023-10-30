c970e1f0d468c76d010f0a90fcae30c29eaf09cc
Vladimir Kravets <natccorp@gmail.com>
2023-10-30 17:13:49 +0200

    Initial Commit

---
+++ FileManager/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  FileManager
+//
+//  Created by Vladimir Kravets on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
